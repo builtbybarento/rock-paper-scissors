db9e8fd2ead0b05eff96c68b8f81640c74f1b9ae
mRezaAnanta <muhammadRezaAnanta@tutamail.com>
2025-01-04 05:42:46 +0700

    feat(choose with img): now we can choose which hand we want to play by using img XD

--- js/main.js
+++ js/main.js
@@ -1,9 +1,26 @@
-document.querySelector('#clickMe').addEventListener('click', makeReq)
+// document.querySelector('#clickMe').addEventListener('click', makeReq)
+let value
+function scissors() {
+  value = 'scissors'
+  makeReq()
+  console.log('scissors')
+}
+function paper() {
+  value = 'paper'
+  makeReq()
+  console.log('paper')
+}
+function rock() {
+  value = 'rock'
+  makeReq()
+  console.log('rock')
+}
 
-async function makeReq(){
-  const playerHand = document.querySelector("#playerHand").value;
+async function makeReq() {
+  // const playerHand = document.querySelector("#playerHand").value;
+  const playerHand = value
   const res = await fetch(`/api`)
-  const data = await res.json() 
+  const data = await res.json()
   let enemyHand = data['enemy-hand'];
   console.log(data)
   console.log(enemyHand)
@@ -11,24 +28,24 @@ async function makeReq(){
   let result = winOrLose(playerHand, enemyHand);
 
   // Print results
-  document.querySelector("#result").textContent = 
+  document.querySelector("#result").textContent =
     `You played ${playerHand}. I played ${enemyHand}. You ${result}.`
 
 
 }
 
-function winOrLose(playerHand, enemyHand){
+function winOrLose(playerHand, enemyHand) {
   // Define relationships 
   const chart = {
-    'rock' : 'scissors',
-    'scissors' : 'paper',
-    'paper' : 'rock'
+    'rock': 'scissors',
+    'scissors': 'paper',
+    'paper': 'rock'
   }
 
   // Check Tie
-  if(playerHand === enemyHand) return "tie";
+  if (playerHand === enemyHand) return "tie";
   // Use dictionary to determine win
-  if(chart[playerHand] === enemyHand) return "win";
+  if (chart[playerHand] === enemyHand) return "win";
   // Not tie or win, must be loss
   return "lose";
 
